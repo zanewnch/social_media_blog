cce044a152d0335398d4610ee6cdccf67aedf4e4
Zane Wang <140219691+zanewnch@users.noreply.github.com>
2025-09-30 10:50:27 +0800

    Refactor HomeComponent and remove ZodiacDetail component
    - Simplified HomeComponent by removing unused methods and signals, streamlining the component's functionality.
    - Updated app.routes.ts to remove the ZodiacDetail route, enhancing navigation.
    - Deleted ZodiacDetail component files (HTML, CSS, TypeScript) to clean up the project structure and reduce complexity.
    - Adjusted home.component.html to reflect new content structure and layout.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ZodiacDetailComponent } from './zodiac-detail/zodiac-detail.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'zodiac-detail/:zodiac', component: ZodiacDetailComponent },
+
   { path: '**', redirectTo: '' }
 ];
